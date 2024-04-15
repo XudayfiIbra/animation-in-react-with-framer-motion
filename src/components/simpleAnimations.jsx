5d9b5f5c0ef97140f5976d674f9a093f9344f16d
xudayfi <xudayfiibraahim8@gmail.com>
2024-04-15 17:26:58 +0300

    Done

--- src/components/simpleAnimations.jsx
+++ src/components/simpleAnimations.jsx
@@ -1,9 +1,16 @@
-import { motion } from "framer-motion";
+import {
+  motion,
+  useAnimation,
+  useInView,
+  useScroll,
+  useTransform,
+} from "framer-motion";
 import { FadeDown } from "./supAnimations/fadeDown";
 import { FadeUp } from "./supAnimations/fadeUp";
 import { KeyFrame } from "./supAnimations/keyFrame";
 import { Button } from "./supAnimations/button";
 import { Scroll } from "./supAnimations/scroll";
+import { useEffect, useRef } from "react";
 
 const gridVariant = {
   hidden: { opacity: 0 },
@@ -15,17 +22,30 @@ const gridVariant = {
   },
 };
 
-
 const gridSquareVariant = {
-    hidden: { opacity: 0 },
-    show: { opacity: 1 },
-  };
-// fade down
-
-// fade up
-
+  hidden: { opacity: 0 },
+  show: { opacity: 1 },
+};
 
 const SimpleAnimations = () => {
+  // ref hook
+  const container = useRef(null);
+  const isInViewPort = useInView(container, { once: true });
+  const controls = useAnimation();
+
+  const { scrollYProgress } = useScroll({
+    target: container,
+    offset: ["start end", "end end"],
+  });
+
+  const p1 = useTransform(scrollYProgress, [0, 1], ["-100%", "0%"]);
+  const p2 = useTransform(scrollYProgress, [0, 1], ["100%", "0%"]);
+
+  useEffect(() => {
+    if (isInViewPort) {
+      controls.start("visible");
+    }
+  }, [isInViewPort]);
   return (
     <>
       <div className="flex flex-col overflow-x-hidden gap-10">
@@ -64,6 +84,32 @@ const SimpleAnimations = () => {
             variants={gridSquareVariant}
             className="bg-slate-800 aspect-square rounded-lg justify-center flex items-center"
           ></motion.div>
+        </motion.section>
+        <motion.section className="flex flex-col gap-10 mb-10" ref={container}>
+          <motion.h1
+            className="text-5xl tracking-wide text-slate-100 text-center"
+            animate={controls}
+            initial="hidden"
+            variants={{
+              hidden: { opacity: 0, y: 75 },
+              visible: {
+                opacity: 1,
+                y: 0,
+              },
+            }}
+            transition={{ delay: 0.3 }}
+          >
+            Lorem ipsum dolor sit
+          </motion.h1>
+
+          <motion.p style={{translateX: p1}} className="text-slate-100 font-thin text-4xl mx-auto w-1/2">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit
+            consequatur tempore dignissimos
+          </motion.p>
+          <motion.p style={{translateX: p2}}  className="text-slate-100 font-thin text-4xl mx-auto w-1/2">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit
+            consequatur tempore dignissimos
+          </motion.p>
         </motion.section>
       </div>
     </>
