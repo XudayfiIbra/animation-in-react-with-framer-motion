86031be06fcc6b5e521a67039b62abecb2c89820
xudayfi <xudayfiibraahim8@gmail.com>
2024-04-23 21:13:23 +0300

    added notifications

--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,12 @@ import App from './App.jsx'
 import './index.css'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Center from './center.jsx'
+import ExampleWrapper from './ModelTest.jsx'
+import SlideInNotifications from './Notivication.jsx'
+import { Example } from './imageMagic.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 
-    <Center />
+    <Example />
 
 )
